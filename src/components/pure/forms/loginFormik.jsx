9701ce3b1e0e937bff0f7532fa96b76e952229c4
betty <mariab.vivanco@gmail.com>
2022-01-04 12:44:38 +0100

    Initial version of login page

---
+++ src/components/pure/forms/loginFormik.jsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import '../../../styles/loginFormix.css'
+
+
+const loginSchema = Yup.object().shape(
+    {
+        email: Yup.string()
+                .email('Invalid email format')
+                .required('Email is required'),
+        password: Yup.string()
+                .required('Password is required')
+    }
+);
+
+
+const Loginformik = () => {
+
+    const initialCredentials = {
+        email: '',
+        password: ''
+    }
+
+    const history = useHistory();
+
+    return (
+        <div>
+                <Formik
+                // *** Initial values that the form will take
+                initialValues = { initialCredentials }
+                // *** Yup Validation Schema ***
+                validationSchema = {loginSchema}
+                // ** onSubmit Event
+                onSubmit={async (values) => {
+                    await new Promise((r) => setTimeout(r, 1000));
+                    alert(JSON.stringify(values, null, 2));
+                    // We save the data in the localstorage
+                    await localStorage.setItem('credentials', values);
+                    history.push('/profile');
+                }}
+            >
+                {/* We obtain props from Formik */}
+                
+                {({ values,
+                    touched,
+                    errors,
+                    isSubmitting,
+                    handleChange,
+                    handleBlur }) => (
+                        <Form className='form'>
+                            
+                            <div class='row' >
+                                <label id="email" for="exampleInputEmail1" >Email</label>
+                            </div>
+                            <div class='row'>
+                                <Field id="emailinput" type="email" name="email" placeholder="Introduce tu correo"  />
+                                {/* Email Errors */}
+                                {errors.email && touched.email && (
+                                    <ErrorMessage name="email" component='div'></ErrorMessage>
+                                )}
+                            
+                            </div>
+                            <div class='row' id="password">
+                                <label htmlFor="password">Contraseña</label>
+                            </div>
+                            <div class='row' >
+                            <Field id="passwordinput"             
+                                name="password"
+                                placeholder="Introduce tu contraseña"
+                                type='password'
+                            />
+                            {/* Password Errors */}
+                            {
+                                errors.password && touched.password && 
+                                (
+                                    <ErrorMessage name="password" component='div'></ErrorMessage>
+                                )
+                            }
+                            </div>
+                            <div class='row' id="otherdata">
+                                <div class="col col-sm-1">
+                                    <div class="form-check">
+                                    <Field type="checkbox" class="form-check-input" id="exampleCheck1" ></Field>
+                                    </div>
+                                    </div>
+                                    <div class="col col-sm-5">
+                                    <label id="remember" class="form-check-label" for="exampleCheck1">Recuérdame</label>
+                                </div>
+                            
+                            <div class="col col-sm-6" id="forgotten" >
+                                He olvidado la contraseña
+                            </div>
+                            </div>
+                            <div class="row">
+                                <button type="submit" id="login" style={{ borderRadius : '8px' }}>Iniciar Sesión</button>
+                                {isSubmitting ? (<p
+                                >Login your credentials...</p>): null}
+                            </div>
+                            
+                            
+                        </Form>
+                )}
+            </Formik>
+        </div>
+    );
+}
+
+export default Loginformik;
---
+++ src/pages/auth/LoginPage.jsx
@@ -0,0 +1,45 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react';
+import Loginformik from '../../components/pure/forms/loginFormik';
+
+
+import '../../styles/Loginpage.css'
+
+const Loginpage = () => {
+    return (
+        <div id="login" >
+                        <div class="row" id="everybody">
+                           
+                            <div class="col-lg-4 col-md-6 col-sm-12 col-xl-4"> 
+                            <div class="row" id='infor'>
+                                    <div id="title">
+                                        <h2 id="title">OpenBootcamp<span id="student">|Alumnos</span></h2>
+                                                                                          
+                                    </div>
+                                    <div class= "row" id="form">
+                                        <Loginformik></Loginformik>
+                                    </div>
+
+                                <div class="row" >
+                                    <div id="copyright">
+                                        <p>
+                                            Copyright © 2021 Open Bootcamp SL, Imagina Group <br />
+                                            Todos los derechos reservados. <br />
+                                            <span><a  id="privacity" href="https://campus.open-vitae.com/"  >Política de Privacidad</a></span>
+                                        </p>
+                                    </div>
+                    
+                                </div> 
+                                </div>
+                            </div>
+                            <div class="col-lg-8 col-md-6 col-sm-0 col-xl-8" id="image">
+                               
+
+                            </div>
+                        </div>
+                        </div> 
+                        
+    );
+}
+
+export default Loginpage;
